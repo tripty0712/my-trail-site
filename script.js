2fed6eb68f30a89c69cea0b6467b8ffd43e9b5cd
Tripty Jain <tripty.jain07@gmail.com>
2021-01-14 20:08:20 -0700

    commit new changes in js file

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = function() {
     var button = document.querySelector('button');
   
     button.addEventListener('click', function() {
-      alert("Let's Rock to Node Programming!!");
+      alert("Let's Rock to Node Programming Guys!!");
     });
   }
