223663d4513752a4ddc0b6f3edc851b2a4113a6b
Tripty Jain <tripty.jain07@gmail.com>
2021-01-18 18:39:10 -0700

    Revert "commit new changes in js file"
    This reverts commit 2fed6eb68f30a89c69cea0b6467b8ffd43e9b5cd.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ window.onload = function() {
     var button = document.querySelector('button');
   
     button.addEventListener('click', function() {
+<<<<<<< HEAD
       alert("Let's Rock Again!!");
+=======
+      alert("Let's Rock to Node Programming!!");
+>>>>>>> parent of 2fed6eb (commit new changes in js file)
     });
   }
